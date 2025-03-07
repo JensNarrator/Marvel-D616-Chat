647f47017fa4c2cd4b14b1c0c0f65126fb84e5ad
JensNarrator <peydrous@gmail.com>
2025-03-07 00:40:47 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAui1jy4dUIcZY7oyr1yh2PrInftj430pM",
+  authDomain: "play-marvel-online.firebaseapp.com",
+  projectId: "play-marvel-online",
+  storageBucket: "play-marvel-online.firebasestorage.app",
+  messagingSenderId: "547670269055",
+  appId: "1:547670269055:web:88ea05fe90d5cf930c334a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
